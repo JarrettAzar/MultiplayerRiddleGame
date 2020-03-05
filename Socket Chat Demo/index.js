f8150acd04f993e88666c6ee0fe822ce24a43c4d
Jarrett Azar <jlock98@gmail.com>
2020-03-05 15:50:48 -0500

    updated to look more like the game

--- Socket Chat Demo/index.js
+++ Socket Chat Demo/index.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var test = '!';
 var found = 'Processing Answer...';
-var answer = '!wow';
+var answer = '!a sponge'; //we are going to want to write something that pre-processes the text given in the answer to make their entry lowercase with no spaces to prevent grammar issues
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -12,12 +12,14 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
     console.log('A User Has Connected');
 
+    io.emit('chat message', 'Please answer this riddle: "I am full of holes but I can still hold water. What am I?"');
+
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
         if(msg.includes(test)){
             io.emit('chat message', found);
             console.log('Attempted Answer:', msg);
-            if(msg.indexOf(answer) !== -1){
+            if(msg.toLowerCase().indexOf(answer.toLowerCase()) !== -1){
                 io.emit('chat message', 'Correct!');
             }
             else {
