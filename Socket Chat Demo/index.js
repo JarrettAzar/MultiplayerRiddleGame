3ec67677808c73df4326a6608848490c1a62095a
Jarrett Azar <jlock98@gmail.com>
2020-03-05 12:56:35 -0500

    Updated to include low level chat bot functionality

--- Socket Chat Demo/index.js
+++ Socket Chat Demo/index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var test = '!';
+var found = 'Processing Answer...';
+var answer = '!wow';
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -11,6 +14,16 @@ io.on('connection', function(socket) {
 
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
+        if(msg.includes(test)){
+            io.emit('chat message', found);
+            console.log('Attempted Answer:', msg);
+            if(msg.indexOf(answer) !== -1){
+                io.emit('chat message', 'Correct!');
+            }
+            else {
+                io.emit('chat message', 'Incorrect, Try Again!');
+            }
+        }
     });
 
     socket.on('disconnect', function() {
